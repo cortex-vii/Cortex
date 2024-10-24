f40d02a74f2b818dad199521fe7559761fd520e7
Pantoja <jvpantojamaciel@gmail.com>
2024-10-24 20:04:26 -0300

    update url endpoint

--- src/app/services/http/endpoints.ts
+++ src/app/services/http/endpoints.ts
@@ -1,5 +1,5 @@
 // app/endpoints.ts
-export const API_BASE_URL = "https://06da-2804-61d0-1062-9001-cd0e-d9e4-fb9b-ab53.ngrok-free.app/api/v1/";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // Função para pegar apenas o que vem após a última barra
 export const cleanHttptUrl = (nextUrl: string | null): string | null => {
